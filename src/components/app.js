a92e52355edddafb8f003b12d6ba4918ca1245dd
Hans Medina <twd2206@gmail.com>
2022-06-30 20:11:51 -0500

    ready list-user

--- src/components/app.js
+++ src/components/app.js
@@ -1,1 +1,52 @@
 "use strict";
+
+class App extends HTMLElement {
+  constructor() {
+    super();
+    this.users = [];
+  }
+
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    const template = document.querySelector("#app-template");
+    const instance = template.content.cloneNode(true);
+    shadowRoot.appendChild(instance);
+    this.saveLocalstore();
+    this.render(shadowRoot);
+  }
+
+  async getUser(page) {
+    await fetch(`https://reqres.in/api/users?page=${page}`)
+      .then((resData) => resData.json())
+      .then(({ data }) => this.users.push(...data));
+  }
+
+  async saveLocalstore() {
+    const data = JSON.parse(localStorage.getItem("dataUsers") || "null");
+    if (data) {
+      this.users = data;
+      return;
+    }
+    // fetch data
+    await this.getUser(1);
+    await this.getUser(2);
+    // add data localstorage
+    localStorage.setItem("dataUsers", JSON.stringify(this.users));
+  }
+
+  render(shadowRoot = new ShadowRoot()) {
+    const list = shadowRoot.querySelector("#user-list");
+    this.users.forEach((user, index) => {
+      const item = document.createElement("item-template");
+      item.setAttribute("data-id", user.id);
+      item.setAttribute("data-first_name", user.first_name);
+      item.setAttribute("data-last_name", user.last_name);
+      item.setAttribute("data-email", user.email);
+      item.setAttribute("data-avatar", user.avatar);
+      item.setAttribute("data-counter", index + 1);
+      list.append(item);
+    });
+  }
+}
+
+customElements.define("app-template", App);
--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ const components = ["app.js", "item.js", "modal.js"];
 // importar componente
 components.forEach((component) => {
   const script = document.createElement("script");
-  script.src = `/src/components/${component}`;
+  script.src = `./src/components/${component}`;
   document.querySelector("head").appendChild(script);
 });
